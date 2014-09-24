dc55065cc307a5e7c3508ad71b8c7c7eece89aeb
monta <djmonta@gmail.com>
2014-09-24 22:20:24 +0900

    Initial Commit

---
+++ resizeSquare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  resizeSquare
+//
+//  Created by 宮本幸子 on 2014/09/24.
+//  Copyright (c) 2014年 Sachiko Miyamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
